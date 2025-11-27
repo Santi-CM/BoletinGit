35a13c316df58bd5c628ecfb5b59e035957ba9c4
Santi-CM <santiagocanadellm@gmail.com>
2025-11-27 12:08:26 +0100

    Calculadora de Notas

--- src/boletinGit/Main.java
+++ src/boletinGit/Main.java
@@ -27,7 +27,26 @@ public class Main {
 // insertar codigo aqui
                     break;
                 case 4:
-// insertar codigo aqui
+
+                    Scanner sc4 = new Scanner(System.in);
+
+                    System.out.println("¿Cuantos alumnos hay en la clase?");
+                    int numeroAlumnos = sc4.nextInt();
+
+                    double nota;
+
+                    double suma = 0;
+
+                    for (int i = 1; i <= numeroAlumnos; i++){
+                        System.out.println("¿Cual es la nota del alumno " + i + "?");
+                        nota = sc4.nextDouble();
+                        suma += nota;
+                    }
+
+                    double media = suma / numeroAlumnos;
+
+                    System.out.println("La media de las notas de la clase es: " + media);
+
                     break;
                 case 0:
                     System.out.println("Saliendo del programa...");
