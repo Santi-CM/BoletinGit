07678e6757b4595cae385c394d94869678424188
juang <juangarridoalcaraz06@gmail.ocm>
2025-11-30 19:47:09 +0100

    w

--- src/boletinGit/Main.java
+++ src/boletinGit/Main.java
@@ -21,27 +21,6 @@ public class Main {
 
         break;
                 case 2:
-                    System.out.println("Introduce un numero entero: ");
-                    int numero = sc.nextInt();
-                    int contadorDivisores=0;
-
-                    if(numero<=2) {
-                        System.out.println("El numero es primo.");
-                    } else if (numero>2) {
-                        for(int i = 2; i<numero;i++) {
-                            if(numero%i==0) {
-                                contadorDivisores++;
-                            }
-
-                        }
-
-                        if(contadorDivisores==0) {
-                            System.out.println("El numero es primo");
-                        } else {
-                            System.out.println("El numero no es primo");
-                        }
-
-                    }
 
 
                     break;
